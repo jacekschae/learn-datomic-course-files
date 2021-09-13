1705145cb00a3af008598d19f34fd4dbc7de6088
Jacek Schae <jacek.schae@gmail.com>
2021-09-13 22:46:57 +0200

    15-wrap-env

---
+++ increments/15-wrap-env/src/test/cheffy/account_tests.clj
@@ -0,0 +1,35 @@
+(ns cheffy.account-tests
+  (:require [clojure.test :refer :all]
+            [cheffy.test-system :as ts]
+            [datomic.client.api :as d]
+            [integrant.repl.state :as state]))
+
+(def db (-> state/system :db/datomic))
+(def conn (:conn db))
+
+(defn account-fixture
+  [f]
+  (ts/create-auth0-test-user
+    {:connection "Username-Password-Authentication"
+     :email "account-tests@cheffy.app"
+     :password "s#m3R4nd0m-pass"})
+  (reset! ts/token (ts/get-test-token "account-tests@cheffy.app"))
+  (f)
+  (d/transact conn {:tx-data [[:db/retractEntity [:account/account-id "account-tests@cheffy.app"]]]})
+  (reset! ts/token nil))
+
+(use-fixtures :once account-fixture)
+
+(deftest account-tests
+
+  (testing "Create user account"
+    (let [{:keys [status]} (ts/test-endpoint :post "/v1/account" {:auth true})]
+      (is (= status 201))))
+
+  (testing "Update user role"
+    (let [{:keys [status]} (ts/test-endpoint :put "/v1/account" {:auth true})]
+      (is (= status 204))))
+
+  (testing "Delete user account"
+    (let [{:keys [status]} (ts/test-endpoint :delete "/v1/account" {:auth true})]
+      (is (= status 204)))))
