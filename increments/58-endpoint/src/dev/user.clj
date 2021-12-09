da5f737c7e0bfac157326acd9d4184242ff4dfdf
Jacek Schae <jacek.schae@gmail.com>
2021-12-09 14:35:49 +0900

    58-endpoint

---
+++ increments/58-endpoint/src/dev/user.clj
@@ -0,0 +1,50 @@
+(ns user
+  (:require [integrant.repl :as ig-repl]
+            [integrant.core :as ig]
+            [integrant.repl.state :as state]
+            [datomic.client.api :as d]))
+
+(ig-repl/set-prep!
+  (fn []
+    (let [config (-> "config/dev.edn" slurp ig/read-string)]
+      (ig/load-namespaces config)
+      config)))
+
+(def start-dev ig-repl/go)
+(def stop-dev ig-repl/halt)
+(def restart-dev (do ig-repl/halt ig-repl/go))
+(def reset-all ig-repl/reset-all)
+
+(def app (-> state/system :cheffy.server/app))
+(def datomic (-> state/system :cheffy.components.datomic-dev-local/db))
+
+(comment
+
+  (cheffy.auth0/get-management-token (-> state/system :cheffy.components.auth0/auth))
+  (ig/load-namespaces
+    (-> "config/dev.edn" slurp ig/read-string))
+
+  (set! *print-namespace-maps* false)
+
+  (d/q '[:find ?e ?v
+         :where
+         [?e :account/account-id ?v]]
+    (d/db (:conn datomic)))
+
+
+  (d/q '[:find ?e ?v ?display-name
+         :in $ ?account-id
+         :where
+         [?e :recipe/recipe-id ?v]
+         [?e :recipe/display-name ?display-name]
+         [?e :recipe/owner ?account-id]]
+    (d/db (:conn datomic)) [:account/account-id "mike@mailinator.com"])
+
+  (d/pull (d/db (:conn datomic)) {:eid [:account/account-id "mike@mailinator.com"]
+                                  :selector '[:account/account-id
+                                              :account/display-name
+                                              {:account/favorite-recipes
+                                               [:recipe/display-name
+                                                :recipe/recipe-id]}]})
+
+  )
---
+++ increments/58-endpoint/src/main/cheffy/recipe/handlers.clj
@@ -0,0 +1,110 @@
+(ns cheffy.recipe.handlers
+  (:require [cheffy.recipe.db :as recipe-db]
+            [cheffy.responses :as responses]
+            [ring.util.response :as rr])
+  (:import (java.util UUID)))
+
+(defn list-all-recipes
+  [{:keys [env claims] :as _request}]
+  (let [account-id (:sub claims)]
+    (rr/response (recipe-db/find-all-recipes (:datomic env) {:account-id account-id}))
+    ))
+
+(defn create-recipe!
+  [{:keys [env claims parameters] :as _request}]
+  (let [recipe-id (UUID/randomUUID)
+        account-id (:sub claims)
+        recipe (:body parameters)]
+    (recipe-db/transact-recipe (:datomic env) (assoc recipe :recipe-id recipe-id :account-id account-id))
+    (rr/created (str responses/base-url "/recipes/" recipe-id) {:recipe-id (str recipe-id)})
+    ))
+
+(defn retrieve-recipe
+  [{:keys [env parameters] :as _request}]
+  (let [recipe-id (-> parameters :path :recipe-id)
+        recipe (recipe-db/find-recipe-by-id (:datomic env) {:recipe-id (UUID/fromString recipe-id)})]
+    (if recipe
+      (rr/response recipe)
+      (rr/not-found {:type "recipe-not-found"
+                     :message "Recipe not found"
+                     :data (str "recipe-id " recipe-id)}))))
+
+(defn update-recipe!
+  [{:keys [env claims parameters] :as _request}]
+  (let [recipe-id (-> parameters :path :recipe-id)
+        account-id (:sub claims)
+        recipe (:body parameters)]
+    (recipe-db/transact-recipe (:datomic env) (assoc recipe :recipe-id (UUID/fromString recipe-id) :account-id account-id))
+    (rr/status 204)
+    ))
+
+(defn delete-recipe!
+  [{:keys [env parameters] :as _request}]
+  (let [recipe-id (-> parameters :path :recipe-id)]
+    (recipe-db/retract-recipe (:datomic env) {:recipe-id (UUID/fromString recipe-id)})
+    (rr/status 204)
+    ))
+
+(defn create-step!
+  [{:keys [env parameters] :as _request}]
+  (let [recipe-id (-> parameters :path :recipe-id)
+        step (:body parameters)
+        step-id (str (UUID/randomUUID))]
+    (recipe-db/transact-step (:datomic env) (assoc step :recipe-id recipe-id
+                                                        :step-id step-id))
+    (rr/created
+      (str responses/base-url "/recipes/" recipe-id)
+      {:step-id step-id})))
+
+(defn update-step!
+  [{:keys [env parameters] :as _request}]
+  (let [step (:body parameters)
+        recipe-id (-> parameters :path :recipe-id)]
+    (recipe-db/transact-step (:datomic env) (assoc step :recipe-id recipe-id))
+    (rr/status 204)))
+
+(defn delete-step!
+  [{:keys [env parameters] :as _request}]
+  (let [step (:body parameters)]
+    (recipe-db/retract-step (:datomic env) step)
+    (rr/status 204)))
+
+(defn create-ingredient!
+  [{:keys [env parameters] :as _request}]
+  (let [recipe-id (-> parameters :path :recipe-id)
+        ingredient (:body parameters)
+        ingredient-id (str (UUID/randomUUID))]
+    (recipe-db/transact-ingredient (:datomic env) (assoc ingredient :recipe-id recipe-id
+                                                                    :ingredient-id ingredient-id))
+    (rr/created
+      (str responses/base-url "/recipes/" recipe-id)
+      {:ingredient-id ingredient-id})))
+
+(defn update-ingredient!
+  [{:keys [env parameters] :as _request}]
+  (let [ingredient (:body parameters)
+        recipe-id (-> parameters :path :recipe-id)]
+    (recipe-db/transact-ingredient (:datomic env) (assoc ingredient :recipe-id recipe-id))
+    (rr/status 204)))
+
+(defn delete-ingredient!
+  [{:keys [env parameters] :as _request}]
+  (let [ingredient (:body parameters)]
+    (recipe-db/retract-ingredient (:datomic env) ingredient)
+    (rr/status 204)))
+
+(defn favorite-recipe!
+  [{:keys [env claims parameters] :as _request}]
+  (let [account-id (:sub claims)
+        recipe-id (-> parameters :path :recipe-id)]
+    (recipe-db/favorite-recipe (:datomic env) {:recipe-id (UUID/fromString recipe-id)
+                                               :account-id account-id})
+    (rr/status 204)))
+
+(defn unfavorite-recipe!
+  [{:keys [env claims parameters] :as _request}]
+  (let [account-id (:sub claims)
+        recipe-id (-> parameters :path :recipe-id)]
+    (recipe-db/unfavorite-recipe (:datomic env) {:recipe-id (UUID/fromString recipe-id)
+                                                 :account-id account-id})
+    (rr/status 204)))
